49feb874d05c5bc482259d4116c1100f86d2a948
KhutuluN <sevda_ergun@eskisehir.edu.tr>
2023-06-15 02:01:16 +0300

    Reservation insert is done.

--- lib/presentation/add_schedule_for_walkers_screen/controller/add_schedule_for_walkers_controller.dart
+++ lib/presentation/add_schedule_for_walkers_screen/controller/add_schedule_for_walkers_controller.dart
@@ -1,7 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:walkwithme/core/app_export.dart';
 import 'package:walkwithme/presentation/add_schedule_for_walkers_screen/models/add_schedule_for_walkers_model.dart';
 
 class AddScheduleForWalkersController extends GetxController {
+
+  TextEditingController titleController = TextEditingController();
+  TextEditingController dogIdController = TextEditingController();
+  TextEditingController startDateController = TextEditingController();
+  TextEditingController endDateController = TextEditingController();
+
   Rx<AddScheduleForWalkersModel> addScheduleForWalkersModelObj =
       AddScheduleForWalkersModel().obs;
 
--- lib/services/db/reservation/reservation_database.dart
+++ lib/services/db/reservation/reservation_database.dart
@@ -1,6 +1,4 @@
-import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:walkwithme/services/db/database_helper.dart';
 
 class ReservationDatabase {
